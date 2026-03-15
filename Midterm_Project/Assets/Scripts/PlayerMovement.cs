52608f0a9acece2a8a63580fd0789e92dbd8a889
DanielTheNerd35 <vazquezd8@montclair.edu>
2026-03-15 15:24:22 -0400

    Overhauled the game

---
+++ Midterm_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Stats")]
+    public float moveSpeed;
+
+    [Header("References")]
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move (InputAction.CallbackContext context)
+    {
+        anim.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("LastInputX", moveInput.x);
+            anim.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        anim.SetFloat("InputX", moveInput.x);
+        anim.SetFloat("InputY", moveInput.y);
+    }
+}
