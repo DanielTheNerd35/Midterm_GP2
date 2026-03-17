efd22e9b283cfe3f98f8d632005392c246b8d694
DanielTheNerd35 <vazquezd8@montclair.edu>
2026-03-17 15:38:34 -0400

    Made a save system

---
+++ Midterm_Project/Assets/Scripts/Saving/SaveData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+
+public class SaveData
+{
+    public Vector3 playerPosition;
+    // \/ that is for remembering map boundaries if I want to do something with that later
+    //public string mapBoundary; 
+}
